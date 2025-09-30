0526248105e6688a789529119649e090c470d6ad
eryxaryxa <eryxaryxa@gmail.com>
2025-09-30 20:27:25 +0700

    Update viplist.lua

--- viplist.lua
+++ viplist.lua
@@ -1,5 +1,6 @@
 return {
     [8858222684] = { permanent = true },
+    [9612905028] = { permanent = true },
     [98765432] = { expiredAt = "2025-12-31" },
     [55555555] = { expiredAt = "2025-10-15" }
 }
