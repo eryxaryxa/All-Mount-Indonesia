0cc122fd2ddb97038a8c48abe6c725f46aad57d9
eryxaryxa <eryxaryxa@gmail.com>
2025-09-30 21:21:11 +0700

    Update viplist.lua

--- viplist.lua
+++ viplist.lua
@@ -1,6 +1,14 @@
 return {
     [8858222684] = { permanent = true },
     [9612905028] = { permanent = true },
-    [98765432] = { expiredAt = "2025-12-31" },
-    [55555555] = { expiredAt = "2025-10-15" }
+    [8286162582] = { permanent = true },
+    [8975946846] = { permanent = true },
+    [9208916374] = { expiredAt = "2025-10-30" },
+    [9542135949] = { expiredAt = "2025-10-30" },
+    [9405046925] = { expiredAt = "2025-10-05" },
+    [8968044449] = { permanent = true },
+    [4886425401] = { permanent = true },
+    [9102230188] = { expiredAt = "2025-10-05" },
+    [1055355357] = { expiredAt = "2025-10-15" },
+    [8920667931] = { expiredAt = "2025-10-30" }
 }
