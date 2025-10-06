1459898f4cdb508fc181e9a965d9d34c66c38fc7
eryxaryxa <eryxaryxa@gmail.com>
2025-10-06 15:21:21 +0700

    Update viplistnew.lua

--- viplistnew.lua
+++ viplistnew.lua
@@ -2,5 +2,5 @@ return {
     [12345678] = { svip = true, permanent = true },
     [87654321] = { gvip = true, expiredAt = "2025-12-31" },
     [11223344] = { vip = true, permanent = true },
-    [8858222684] = { gvip = true, permanent = true }
+    [8858222684] = { svip = true, permanent = true }
 }
