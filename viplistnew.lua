b39345687bf47d982597dc18689743bf257f9a11
eryxaryxa <eryxaryxa@gmail.com>
2025-10-12 01:47:38 +0700

    viplistnew.lua

--- viplistnew.lua
+++ viplistnew.lua
@@ -114,5 +114,6 @@ return {
     [9093242681] = { svip = true, expiredAt = "2025-10-26" },
     [8499519488] = { vip = true, expiredAt = "2025-10-26" },
     [4728757604] = { svip = true, permanent = true },
-    [8361073659] = { svip = true, permanent= true }
+    [8361073659] = { svip = true, permanent= true },
+    [9412346898] = { svip = true, permanent= true }
 }
