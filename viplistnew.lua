f690d8d3129764005493dda03f6db7fbb610987c
Er. <eryxaryxa@gmail.com>
2025-12-18 23:05:00 +0700

    Update viplistnew.lua

--- viplistnew.lua
+++ viplistnew.lua
@@ -81,5 +81,6 @@ return {
     [8928550762] = { kvip = true, expiredAt = "2026-01-06" },
     [9719560312] = { kvip = true, expiredAt = "2026-01-08" },
     [8802951904] = { gvip = true, permanent = true },
-    [5840493713] = { svip = true, expiredAt = "2026-01-07" }
+    [5840493713] = { svip = true, expiredAt = "2026-01-07" },
+    [9503285828] = { svip = true, expiredAt = "2026-01-18" }
 }
