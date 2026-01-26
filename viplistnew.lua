eed3c251693bb07d93710e3e520db9072632b41e
Er. <eryxaryxa@gmail.com>
2026-01-26 16:29:44 +0700

    Update viplistnew.lua

--- viplistnew.lua
+++ viplistnew.lua
@@ -76,7 +76,6 @@ return {
     [8739869471] = { gvip = true, expiredAt = "2025-12-14" },
     [4925166700] = { svip = true, expiredAt = "2025-12-30" },
     [9478506540] = { gvip = true, expiredAt = "2025-12-10" },
-    [8980772767] = { svip = true, expiredAt = "2026-01-02" },
     [8928550762] = { vip = true, expiredAt = "2026-01-06" },
     [9719560312] = { vip = true, expiredAt = "2026-01-08" },
     [8802951904] = { gvip = true, permanent = true },
